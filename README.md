008d31823153e98892dd314d2acaccae9b833e6d
Erik Fröjdh <erik.frojdh@gmail.com>
2024-10-28 16:24:02 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Using AARE
+# Using AARE
+
+```bash
+#Assuming that aare is installed in SOME_PATH
+git clone https://github.com/slsdetectorgroup/aare-examples.git
+cd aare-examples
+mkdir build
+cd build
+cmake .. cmake .. -DCMAKE_PREFIX_PATH=SOME_PATH
+make -j4
+
+#test the program
+$ ./file_info path/to/my/file_master_0.json
+Frames: 10, Rows: 512, cols: 1024
+
+```
--- file_info.cpp
+++ file_info.cpp
@@ -1,21 +1,23 @@
 #include "aare/File.hpp"
+
 #include <fmt/core.h>
 #include <fmt/color.h>
-#include <filesystem>
 
 int main(int argc, char const *argv[]){
+    //First argument is the name of the program, as second we expect the filename
     if (argc < 2){
         fmt::print(fg(fmt::terminal_color::magenta), "Usage: {} <filename>\n", argv[0]);
         return 1;
     }
 
+    //Wrap the code in a try-catch block since we don't know if the file exists
     try{
         aare::File f(argv[1], "r");
         fmt::print("Frames: {}, Rows: {}, cols: {}\n", f.total_frames(), f.rows(), f.cols());
+        return 0;
     } catch (const std::exception &e){
         fmt::print(fg(fmt::terminal_color::red), "Error: {}\n", e.what());
+        return 1;
     }
-
     
-    return 0;
 }
